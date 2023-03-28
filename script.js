810be67daa5f5ea8c94498ee28f6c656b9291930
ukiy87 <97949370+ukiy87@users.noreply.github.com>
2023-03-28 08:47:33 +0300

    Add files via upload
    pievienojām funkcijas

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const plusins = document.querySelector('.js-plus');
 const inputins = document.querySelector('.js-input');
 
-console.log(plusins)
-console.log(inputins)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("Maini nospieda")
+})
